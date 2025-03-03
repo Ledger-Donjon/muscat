ae79be4f427d2ed7a58fb6170f04f98824bb6693
TrAyZeN <leo.benito@ledger.fr>
2025-03-03 16:24:22 +0100

    Add processors to muscatpy

--- muscatpy/src/lib.rs
+++ muscatpy/src/lib.rs
@@ -3,6 +3,7 @@ use pyo3::prelude::*;
 mod distinguishers;
 mod leakage_detection;
 mod leakage_model;
+mod processors;
 
 #[pymodule]
 fn muscatpy(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -17,6 +18,10 @@ fn muscatpy(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     let leakage_model_module = PyModule::new(py, "leakage_model")?;
     leakage_model::leakage_model(py, &leakage_model_module)?;
     m.add_submodule(&leakage_model_module)?;
+
+    let processors_module = PyModule::new(py, "processors")?;
+    processors::processors(py, &processors_module)?;
+    m.add_submodule(&processors_module)?;
 
     Ok(())
 }
---
+++ muscatpy/src/processors.rs
@@ -0,0 +1,41 @@
+use muscat::processors::MeanVar;
+use numpy::{IntoPyArray, PyArray1, PyArray2, PyArrayMethods};
+use pyo3::prelude::*;
+
+#[pyfunction]
+pub fn compute_mean<'py>(traces: &Bound<'py, PyArray2<i64>>) -> Bound<'py, PyArray1<f64>> {
+    let py = traces.py();
+
+    let traces_ro = traces.readonly();
+    let traces = traces_ro.as_array();
+
+    let mut meanvar = MeanVar::new(traces.shape()[1]);
+    for trace in traces.rows() {
+        meanvar.process(trace);
+    }
+
+    meanvar.mean().into_pyarray(py)
+}
+
+#[pyfunction]
+pub fn compute_var<'py>(traces: &Bound<'py, PyArray2<i64>>) -> Bound<'py, PyArray1<f64>> {
+    let py = traces.py();
+
+    let traces_ro = traces.readonly();
+    let traces = traces_ro.as_array();
+
+    let mut meanvar = MeanVar::new(traces.shape()[1]);
+    for trace in traces.rows() {
+        meanvar.process(trace);
+    }
+
+    meanvar.var().into_pyarray(py)
+}
+
+#[pymodule]
+pub fn processors(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(compute_mean, m)?)?;
+    m.add_function(wrap_pyfunction!(compute_var, m)?)?;
+
+    Ok(())
+}
