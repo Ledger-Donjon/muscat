d53425b68134221110a7216e8544da49c98d57ff
TrAyZeN <leo.benito@ledger.fr>
2025-03-03 17:11:46 +0100

    Dispatch muscatpy calls based on dtype

--- muscatpy/src/leakage_detection.rs
+++ muscatpy/src/leakage_detection.rs
@@ -1,48 +1,100 @@
 use numpy::{
     array::{PyArray1, PyArray2},
+    dtype,
     ndarray::Array1,
-    IntoPyArray, PyArrayMethods,
+    IntoPyArray, PyArrayDescrMethods, PyArrayMethods, PyUntypedArray, PyUntypedArrayMethods,
 };
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::types::PyFunction;
 
 #[pyfunction]
 pub fn compute_snr<'py>(
-    leakages: &Bound<'py, PyArray2<i64>>,
+    traces: &Bound<'py, PyUntypedArray>,
     classes: usize,
     get_class: &Bound<'py, PyFunction>,
     batch_size: usize,
-) -> Bound<'py, PyArray1<f32>> {
-    let mut leakages_class = Array1::zeros(leakages.readonly().as_array().len());
-    for i in 0..leakages_class.len() {
-        leakages_class[i] = get_class
+) -> PyResult<Bound<'py, PyArray1<f32>>> {
+    let mut traces_class = Array1::zeros(traces.len());
+    for i in 0..traces_class.len() {
+        traces_class[i] = get_class
             .call((i,), None)
             .unwrap()
             .extract::<usize>()
             .unwrap();
     }
 
-    muscat::leakage_detection::snr(
-        leakages.readonly().as_array(),
-        classes,
-        |i| leakages_class[i],
-        batch_size,
-    )
-    .into_pyarray(leakages.py())
+    if traces.ndim() != 2 {
+        return Err(PyTypeError::new_err(format!(
+            "Invalid traces ndim: {}. traces ndim should be equal to 2.",
+            traces.ndim()
+        )));
+    }
+
+    let sample_dtype = traces.dtype();
+
+    macro_rules! type_dispatch {
+        ($($ty:ty),*) => {
+            $(
+                if sample_dtype.is_equiv_to(&dtype::<$ty>(traces.py())) {
+                    let traces = traces.downcast::<PyArray2<$ty>>()?;
+
+                    return Ok(muscat::leakage_detection::snr(
+                        traces.readonly().as_array(),
+                        classes,
+                        |i| traces_class[i],
+                        batch_size,
+                    ).into_pyarray(traces.py()));
+                }
+            )*
+        };
+    }
+
+    type_dispatch! { u8, u16, u32, u64, i8, i16, i32, i64, f32 }
+
+    Err(PyTypeError::new_err(format!(
+        "Unsupported traces dtype: {}.",
+        sample_dtype
+    )))
 }
 
 #[pyfunction]
 pub fn compute_ttest<'py>(
-    traces: &Bound<'py, PyArray2<i64>>,
+    traces: &Bound<'py, PyUntypedArray>,
     trace_classes: &Bound<'py, PyArray1<bool>>,
     batch_size: usize,
-) -> Bound<'py, PyArray1<f32>> {
-    muscat::leakage_detection::ttest(
-        traces.readonly().as_array(),
-        trace_classes.readonly().as_array(),
-        batch_size,
-    )
-    .into_pyarray(traces.py())
+) -> PyResult<Bound<'py, PyArray1<f32>>> {
+    if traces.ndim() != 2 {
+        return Err(PyTypeError::new_err(format!(
+            "Invalid traces ndim: {}. traces ndim should be equal to 2.",
+            traces.ndim()
+        )));
+    }
+
+    let sample_dtype = traces.dtype();
+
+    macro_rules! type_dispatch {
+        ($($ty:ty),*) => {
+            $(
+                if sample_dtype.is_equiv_to(&dtype::<$ty>(traces.py())) {
+                    let traces = traces.downcast::<PyArray2<$ty>>()?;
+
+                    return Ok(muscat::leakage_detection::ttest(
+                        traces.readonly().as_array(),
+                        trace_classes.readonly().as_array(),
+                        batch_size,
+                    ).into_pyarray(traces.py()));
+                }
+            )*
+        };
+    }
+
+    type_dispatch! { u8, u16, u32, u64, i8, i16, i32, i64, f32 }
+
+    Err(PyTypeError::new_err(format!(
+        "Unsupported traces dtype: {}.",
+        sample_dtype
+    )))
 }
 
 #[pymodule]
--- muscatpy/src/processors.rs
+++ muscatpy/src/processors.rs
@@ -1,35 +1,95 @@
 use muscat::processors::MeanVar;
-use numpy::{IntoPyArray, PyArray1, PyArray2, PyArrayMethods};
+use numpy::{
+    dtype, IntoPyArray, PyArray1, PyArray2, PyArrayDescrMethods, PyArrayMethods, PyUntypedArray,
+    PyUntypedArrayMethods,
+};
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 
 #[pyfunction]
-pub fn compute_mean<'py>(traces: &Bound<'py, PyArray2<i64>>) -> Bound<'py, PyArray1<f32>> {
+pub fn compute_mean<'py>(
+    traces: &Bound<'py, PyUntypedArray>,
+) -> PyResult<Bound<'py, PyArray1<f32>>> {
     let py = traces.py();
 
-    let traces_ro = traces.readonly();
-    let traces = traces_ro.as_array();
-
-    let mut meanvar = MeanVar::new(traces.shape()[1]);
-    for trace in traces.rows() {
-        meanvar.process(trace);
+    if traces.ndim() != 2 {
+        return Err(PyTypeError::new_err(format!(
+            "Invalid traces ndim: {}. traces ndim should be equal to 2.",
+            traces.ndim()
+        )));
     }
 
-    meanvar.mean().into_pyarray(py)
+    let sample_dtype = traces.dtype();
+
+    macro_rules! type_dispatch {
+        ($($ty:ty),*) => {
+            $(
+                if sample_dtype.is_equiv_to(&dtype::<$ty>(traces.py())) {
+                    let traces = traces.downcast::<PyArray2<$ty>>()?;
+
+                    let traces_ro = traces.readonly();
+                    let traces = traces_ro.as_array();
+
+                    let mut meanvar = MeanVar::new(traces.shape()[1]);
+                    for trace in traces.rows() {
+                        meanvar.process(trace);
+                    }
+
+                    return Ok(meanvar.mean().into_pyarray(py));
+                }
+            )*
+        };
+    }
+
+    type_dispatch! { u8, u16, u32, u64, i8, i16, i32, i64, f32 }
+
+    Err(PyTypeError::new_err(format!(
+        "Unsupported traces dtype: {}.",
+        sample_dtype
+    )))
 }
 
 #[pyfunction]
-pub fn compute_var<'py>(traces: &Bound<'py, PyArray2<i64>>) -> Bound<'py, PyArray1<f32>> {
+pub fn compute_var<'py>(
+    traces: &Bound<'py, PyUntypedArray>,
+) -> PyResult<Bound<'py, PyArray1<f32>>> {
     let py = traces.py();
 
-    let traces_ro = traces.readonly();
-    let traces = traces_ro.as_array();
-
-    let mut meanvar = MeanVar::new(traces.shape()[1]);
-    for trace in traces.rows() {
-        meanvar.process(trace);
+    if traces.ndim() != 2 {
+        return Err(PyTypeError::new_err(format!(
+            "Invalid traces ndim: {}. traces ndim should be equal to 2.",
+            traces.ndim()
+        )));
     }
 
-    meanvar.var().into_pyarray(py)
+    let sample_dtype = traces.dtype();
+
+    macro_rules! type_dispatch {
+        ($($ty:ty),*) => {
+            $(
+                if sample_dtype.is_equiv_to(&dtype::<$ty>(traces.py())) {
+                    let traces = traces.downcast::<PyArray2<$ty>>()?;
+
+                    let traces_ro = traces.readonly();
+                    let traces = traces_ro.as_array();
+
+                    let mut meanvar = MeanVar::new(traces.shape()[1]);
+                    for trace in traces.rows() {
+                        meanvar.process(trace);
+                    }
+
+                    return Ok(meanvar.var().into_pyarray(py));
+                }
+            )*
+        };
+    }
+
+    type_dispatch! { u8, u16, u32, u64, i8, i16, i32, i64, f32 }
+
+    Err(PyTypeError::new_err(format!(
+        "Unsupported traces dtype: {}.",
+        sample_dtype
+    )))
 }
 
 #[pymodule]
