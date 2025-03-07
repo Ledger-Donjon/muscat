1f0aa39eb73fb52cdc48b5255fd7c99fcbaed59d
TrAyZeN <leo.benito@ledger.fr>
2025-03-07 15:40:31 +0100

    Add generic trace type annotations

--- muscatpy/muscatpy/leakage_detection.pyi
+++ muscatpy/muscatpy/leakage_detection.pyi
@@ -1,19 +1,32 @@
 from collections.abc import Callable
+from typing import Union
 
 import numpy as np
 import numpy.typing as npt
 
+Trace = Union[
+    npt.NDArray[np.uint8],
+    npt.NDArray[np.uint16],
+    npt.NDArray[np.uint32],
+    npt.NDArray[np.uint64],
+    npt.NDArray[np.int8],
+    npt.NDArray[np.int16],
+    npt.NDArray[np.int32],
+    npt.NDArray[np.int64],
+    npt.NDArray[np.float32],
+]
+
 def compute_snr(
-    leakages: npt.NDArray[np.int64],
+    traces: Trace,
     classes: int,
     get_class: Callable[[int], int],
     batch_size: int,
-) -> npt.NDArray[np.float64]:
+) -> npt.NDArray[np.float32]:
     """Compute the SNR of the given traces."""
 
 def compute_ttest(
-    traces: npt.NDArray[np.int64],
+    traces: Trace,
     trace_classes: npt.NDArray[np.bool_],
     batch_size: int,
-) -> npt.NDArray[np.float64]:
+) -> npt.NDArray[np.float32]:
     """Compute the Welch's T-test of the given traces."""
