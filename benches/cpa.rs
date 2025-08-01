eb34698b74d570b28d8b0c6d43ad610d007f3176
TrAyZeN <leo.benito@ledger.fr>
2025-08-01 13:35:54 +0200

    Benchmark finalize implementations

--- benches/cpa.rs
+++ benches/cpa.rs
@@ -1,11 +1,15 @@
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use muscat::Sample;
 use muscat::distinguishers::cpa::{self, Cpa, CpaProcessor};
 use muscat::distinguishers::cpa_normal;
 use muscat::leakage_model::{aes::sbox, hw};
-use ndarray::{Array2, ArrayView1, Axis};
+use ndarray::{Array1, Array2, ArrayView1, Axis};
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand::{SeedableRng, rngs::StdRng};
 use ndarray_rand::rand_distr::Uniform;
+use num_traits::AsPrimitive;
+use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
+use serde::{Deserialize, Serialize};
 use std::iter::zip;
 
 pub fn leakage_model(plaintext: usize, guess: usize) -> usize {
@@ -114,5 +118,260 @@ fn bench_cpa(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_cpa);
+fn bench_finalize(c: &mut Criterion) {
+    // Seed rng to get the same output each run
+    let mut rng = StdRng::seed_from_u64(0);
+
+    let mut group = c.benchmark_group("cpa_finalize");
+
+    group.measurement_time(std::time::Duration::from_secs(60));
+
+    for num_traces in [5000, 10000, 25000].into_iter() {
+        let traces = Array2::random_using((num_traces, 5000), Uniform::new(-2., 2.), &mut rng);
+        let plaintexts = Array2::random_using(
+            (num_traces, 16),
+            Uniform::new_inclusive(0u8, 255u8),
+            &mut rng,
+        );
+
+        let mut cpa = CpaProcessorFinalize::new(traces.shape()[1], 256);
+
+        for i in 0..traces.shape()[0] {
+            cpa.update(traces.row(i), plaintexts.row(i)[0] as usize, leakage_model);
+        }
+
+        group.bench_function(BenchmarkId::new("finalize", num_traces), |b| {
+            b.iter(|| cpa.finalize(leakage_model))
+        });
+
+        group.bench_function(BenchmarkId::new("finalize_nopar", num_traces), |b| {
+            b.iter(|| cpa.finalize_nopar(leakage_model))
+        });
+
+        group.bench_function(BenchmarkId::new("finalize_par_guess", num_traces), |b| {
+            b.iter(|| cpa.finalize_par_guess(leakage_model))
+        });
+    }
+
+    group.finish();
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CpaProcessorFinalize<T>
+where
+    T: Sample,
+{
+    /// Number of samples per trace
+    num_samples: usize,
+    /// Guess range upper excluded bound
+    guess_range: usize,
+    /// Sum of traces
+    #[serde(bound(serialize = "<T as Sample>::Container: Serialize"))]
+    #[serde(bound(deserialize = "<T as Sample>::Container: Deserialize<'de>"))]
+    sum_traces: Array1<<T as Sample>::Container>,
+    /// Sum of square of traces
+    #[serde(bound(serialize = "<T as Sample>::Container: Serialize"))]
+    #[serde(bound(deserialize = "<T as Sample>::Container: Deserialize<'de>"))]
+    sum_square_traces: Array1<<T as Sample>::Container>,
+    /// Sum of traces per key guess
+    guess_sum_traces: Array1<usize>,
+    /// Sum of square of traces per key guess
+    guess_sum_squares_traces: Array1<usize>,
+    /// Sum of traces per plaintext used
+    /// See 4.3 in <https://eprint.iacr.org/2013/794.pdf>
+    #[serde(bound(serialize = "<T as Sample>::Container: Serialize"))]
+    #[serde(bound(deserialize = "<T as Sample>::Container: Deserialize<'de>"))]
+    plaintext_sum_traces: Array2<<T as Sample>::Container>,
+    /// Number of traces processed
+    num_traces: usize,
+}
+
+impl<T> CpaProcessorFinalize<T>
+where
+    T: Sample + Copy,
+    <T as Sample>::Container: Sync,
+{
+    pub fn new(num_samples: usize, guess_range: usize) -> Self {
+        Self {
+            num_samples,
+            guess_range,
+            sum_traces: Array1::zeros(num_samples),
+            sum_square_traces: Array1::zeros(num_samples),
+            guess_sum_traces: Array1::zeros(guess_range),
+            guess_sum_squares_traces: Array1::zeros(guess_range),
+            plaintext_sum_traces: Array2::zeros((guess_range, num_samples)),
+            num_traces: 0,
+        }
+    }
+
+    /// # Panics
+    /// Panic in debug if `trace.shape()[0] != self.num_samples`.
+    pub fn update<P, F>(&mut self, trace: ArrayView1<T>, plaintext: P, leakage_model: F)
+    where
+        P: Into<usize> + Copy,
+        F: Fn(usize, usize) -> usize,
+    {
+        debug_assert_eq!(trace.shape()[0], self.num_samples);
+
+        let plaintext = plaintext.into();
+        for i in 0..self.num_samples {
+            let t = trace[i].into();
+
+            self.sum_traces[i] += t;
+            self.sum_square_traces[i] += t * t;
+
+            self.plaintext_sum_traces[[plaintext, i]] += t;
+        }
+
+        for guess in 0..self.guess_range {
+            let value = leakage_model(plaintext, guess);
+            self.guess_sum_traces[guess] += value;
+            self.guess_sum_squares_traces[guess] += value * value;
+        }
+
+        self.num_traces += 1;
+    }
+
+    /// Finalize the calculation after feeding the overall traces.
+    pub fn finalize<F>(&self, leakage_model: F) -> Array2<f32>
+    where
+        F: Fn(usize, usize) -> usize,
+    {
+        let mut modeled_leakages = Array1::zeros(self.guess_range);
+
+        let mut corr = Array2::zeros((self.guess_range, self.num_samples));
+        for guess in 0..self.guess_range {
+            for u in 0..self.guess_range {
+                modeled_leakages[u] = leakage_model(u, guess) as f32;
+            }
+
+            let mean_key = self.guess_sum_traces[guess] as f32 / self.num_traces as f32;
+            let mean_squares_key =
+                self.guess_sum_squares_traces[guess] as f32 / self.num_traces as f32;
+            let var_key = mean_squares_key - (mean_key * mean_key);
+
+            let guess_corr: Vec<_> = (0..self.num_samples)
+                .into_par_iter()
+                .map(|u| {
+                    let mean_traces = self.sum_traces[u].as_() / self.num_traces as f32;
+
+                    let cov = self
+                        .plaintext_sum_traces
+                        .column(u)
+                        .mapv(|x| x.as_())
+                        .dot(&modeled_leakages.view());
+                    let cov = cov / self.num_traces as f32 - (mean_key * mean_traces);
+
+                    let mean_squares_traces =
+                        self.sum_square_traces[u].as_() / self.num_traces as f32;
+                    let var_traces = mean_squares_traces - (mean_traces * mean_traces);
+                    f32::abs(cov / f32::sqrt(var_key * var_traces))
+                })
+                .collect();
+
+            #[allow(clippy::needless_range_loop)]
+            for u in 0..self.num_samples {
+                corr[[guess, u]] = guess_corr[u];
+            }
+        }
+
+        corr
+    }
+
+    /// Finalize the calculation after feeding the overall traces.
+    pub fn finalize_nopar<F>(&self, leakage_model: F) -> Array2<f32>
+    where
+        F: Fn(usize, usize) -> usize,
+    {
+        let mut modeled_leakages = Array1::zeros(self.guess_range);
+
+        let mut corr = Array2::zeros((self.guess_range, self.num_samples));
+        for guess in 0..self.guess_range {
+            for u in 0..self.guess_range {
+                modeled_leakages[u] = leakage_model(u, guess) as f32;
+            }
+
+            let mean_key = self.guess_sum_traces[guess] as f32 / self.num_traces as f32;
+            let mean_squares_key =
+                self.guess_sum_squares_traces[guess] as f32 / self.num_traces as f32;
+            let var_key = mean_squares_key - (mean_key * mean_key);
+
+            let guess_corr: Vec<_> = (0..self.num_samples)
+                .map(|u| {
+                    let mean_traces = self.sum_traces[u].as_() / self.num_traces as f32;
+
+                    let cov = self
+                        .plaintext_sum_traces
+                        .column(u)
+                        .mapv(|x| x.as_())
+                        .dot(&modeled_leakages.view());
+                    let cov = cov / self.num_traces as f32 - (mean_key * mean_traces);
+
+                    let mean_squares_traces =
+                        self.sum_square_traces[u].as_() / self.num_traces as f32;
+                    let var_traces = mean_squares_traces - (mean_traces * mean_traces);
+                    f32::abs(cov / f32::sqrt(var_key * var_traces))
+                })
+                .collect();
+
+            #[allow(clippy::needless_range_loop)]
+            for u in 0..self.num_samples {
+                corr[[guess, u]] = guess_corr[u];
+            }
+        }
+
+        corr
+    }
+
+    /// Finalize the calculation after feeding the overall traces.
+    pub fn finalize_par_guess<F>(&self, leakage_model: F) -> Array2<f32>
+    where
+        F: Fn(usize, usize) -> usize + Sync,
+    {
+        let mut corr = Array2::zeros((self.guess_range, self.num_samples));
+        corr.axis_iter_mut(Axis(0))
+            .enumerate()
+            .par_bridge()
+            // WARN: Not sure about the mut here
+            .for_each(|(guess, mut corr_row)| {
+                let mut modeled_leakages = Array1::zeros(self.guess_range);
+
+                for u in 0..self.guess_range {
+                    modeled_leakages[u] = leakage_model(u, guess) as f32;
+                }
+
+                let mean_key = self.guess_sum_traces[guess] as f32 / self.num_traces as f32;
+                let mean_squares_key =
+                    self.guess_sum_squares_traces[guess] as f32 / self.num_traces as f32;
+                let var_key = mean_squares_key - (mean_key * mean_key);
+
+                let guess_corr: Vec<_> = (0..self.num_samples)
+                    .map(|u| {
+                        let mean_traces = self.sum_traces[u].as_() / self.num_traces as f32;
+
+                        let cov = self
+                            .plaintext_sum_traces
+                            .column(u)
+                            .mapv(|x| x.as_())
+                            .dot(&modeled_leakages.view());
+                        let cov = cov / self.num_traces as f32 - (mean_key * mean_traces);
+
+                        let mean_squares_traces =
+                            self.sum_square_traces[u].as_() / self.num_traces as f32;
+                        let var_traces = mean_squares_traces - (mean_traces * mean_traces);
+                        f32::abs(cov / f32::sqrt(var_key * var_traces))
+                    })
+                    .collect();
+
+                #[allow(clippy::needless_range_loop)]
+                for u in 0..self.num_samples {
+                    corr_row[u] = guess_corr[u];
+                }
+            });
+
+        corr
+    }
+}
+
+criterion_group!(benches, bench_cpa, bench_finalize);
 criterion_main!(benches);
